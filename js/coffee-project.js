6f60047b761b887a019e7dec4f29795613f26165
guiji2007 <98149176+guiji2007@users.noreply.github.com>
2023-11-15 16:38:18 -0600

    form-actions to create a new coffee

--- js/coffee-project.js
+++ js/coffee-project.js
@@ -1,43 +1,45 @@
 import { coffees } from "./coffees.js";
 
-const handleNewCoffeeBtn = () =>{
-    const btnCreateForm = document.querySelector(`#addCoffee`);
-    const coffeeForm = document.querySelector(`.coffee-form`)
+// Ask customer to create new coffees
+const handleNewCoffeeBtn = () => {
+  const btnCreateForm = document.querySelector(`#addCoffee`);
+  const coffeeForm = document.querySelector(`.coffee-form`);
 
-    btnCreateForm.addEventListener(`click`,(e)=>{
-        coffeeForm.classList.toggle('display-form');
-    });
-}
+  btnCreateForm.addEventListener(`click`, (e) => {
+    coffeeForm.classList.toggle("display-form");
+  });
+};
 
-const createCoffee =(coffee)=>{
+const createCoffee = () => {
+  let coffee = {};
 
-    coffee.id = coffees.length + 1;
-    const coffeeName = document.querySelector(`#coffee-name`);
-    coffeeName.value = coffee.name;
-    const coffeeRoast = document.querySelector(`#coffee-roast`);
-    coffeeRoast.value = coffee.roast;
-    const coffeeDesc = document.querySelector(`#coffee-desc`);
-    coffeeDesc.value = coffee.desc;
-    const coffeePrice = document.querySelector(`#coffee-price`);
-    coffeePrice.value = coffee.price;
-    const coffeeLink = document.querySelector(`#coffee-link`);
-    coffeeLink.value = coffee.src;
+  coffee.id = coffees.length + 1;
+  const coffeeName = document.querySelector(`#coffee-name`);
+  coffee.name = coffeeName.value;
+  const coffeeRoast = document.querySelector(`#coffee-roast`);
+  coffee.roast = coffeeRoast.value;
+  const coffeeDesc = document.querySelector(`#coffee-desc`);
+  coffee.desc = coffeeDesc.value;
+  const coffeePrice = document.querySelector(`#coffee-price`);
+  coffee.price = coffeePrice.value;
+  const coffeeLink = document.querySelector(`#coffee-link`);
+  coffee.src = coffeeLink.value;
 
-    coffees.push(coffee);
-}
+  coffees.push(coffee);
+  console.log(coffee);
+};
 
-const handleSubmitBtn = (coffee) =>{
-    const submitCoffeeBtn = document.querySelector(`.submitCoffee`);
+const handleSubmitBtn = () => {
+  const submitCoffeeBtn = document.querySelector(`.submitCoffee`);
 
-    submitCoffeeBtn.addEventListener('click', (e)=>{
-        createCoffee(coffee);
-    });
-}
+  submitCoffeeBtn.addEventListener("click", (e) => {
+    createCoffee();
+  });
+};
 
+// Create new coffee card to HTML!!!
 
-
-(()=>{
-handleNewCoffeeBtn();
-;
-handleSubmitBtn(coffee);
+(() => {
+  handleNewCoffeeBtn();
+  handleSubmitBtn();
 })();
