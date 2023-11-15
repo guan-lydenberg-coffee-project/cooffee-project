83f2e8fa99859dcefd5a525bc6b62562ac34d399
Casper <clydenberg@gmail.com>
2023-11-15 15:44:41 -0600

    making progress

--- js/coffee-project.js
+++ js/coffee-project.js
@@ -1,8 +1,15 @@
 import { coffees } from "./coffees.js";
 
+const handleNewCoffeeBtn = () =>{
+    const btnCreateForm = document.querySelector(`#addCoffee`);
+    const coffeeForm = document.querySelector(`.coffee-form`)
 
+    btnCreateForm.addEventListener(`click`,(e)=>{
+        coffeeForm.classList.toggle('display-form');
+    });
+}
 
 
 (()=>{
-
+handleNewCoffeeBtn();
 })();
