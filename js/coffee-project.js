35cae45dc2470f3faddeeb33f52763d92c6f7d7c
Casper <clydenberg@gmail.com>
2023-11-15 16:23:39 -0600

    jiabcuhebs

--- js/coffee-project.js
+++ js/coffee-project.js
@@ -9,7 +9,35 @@ const handleNewCoffeeBtn = () =>{
     });
 }
 
+const createCoffee =(coffee)=>{
+
+    coffee.id = coffees.length + 1;
+    const coffeeName = document.querySelector(`#coffee-name`);
+    coffeeName.value = coffee.name;
+    const coffeeRoast = document.querySelector(`#coffee-roast`);
+    coffeeRoast.value = coffee.roast;
+    const coffeeDesc = document.querySelector(`#coffee-desc`);
+    coffeeDesc.value = coffee.desc;
+    const coffeePrice = document.querySelector(`#coffee-price`);
+    coffeePrice.value = coffee.price;
+    const coffeeLink = document.querySelector(`#coffee-link`);
+    coffeeLink.value = coffee.src;
+
+    coffees.push(coffee);
+}
+
+const handleSubmitBtn = (coffee) =>{
+    const submitCoffeeBtn = document.querySelector(`.submitCoffee`);
+
+    submitCoffeeBtn.addEventListener('click', (e)=>{
+        createCoffee(coffee);
+    });
+}
+
+
 
 (()=>{
 handleNewCoffeeBtn();
+;
+handleSubmitBtn(coffee);
 })();
